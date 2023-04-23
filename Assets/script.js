e18e9eddb2c0c2a1c79ace1e182daa9d44f51c4b
Stephanie <126029841+HarrisSte@users.noreply.github.com>
2023-04-23 18:31:43 -0400

    Working on getting weather, formatting, etc

--- Assets/script.js
+++ Assets/script.js
@@ -1,31 +1,30 @@
 //Variables & days for weather
 var currentDate = dayjs().format("YYYY-MM-DD");
-var day1 = dayjs().format("YYYY-MM-DD");
-var day2 = dayjs().format("YYYY-MM-DD");
-var day3 = dayjs().format("YYYY-MM-DD");
-var day4 = dayjs().format("YYYY-MM-DD");
-var day5 = dayjs().format("YYYY-MM-DD");
+var day1 = dayjs().format(1, "YYYY-MM-DD");
+var day2 = dayjs().format(2, "YYYY-MM-DD");
+var day3 = dayjs().format(3, "YYYY-MM-DD");
+var day4 = dayjs().format(4, "YYYY-MM-DD");
+var day5 = dayjs().format(5, "YYYY-MM-DD");
 
-function getWeather() {
-  var apiKey = "9e7d1b0a51b2cdd53ceb30a255a08f48";
-  var location = document.getElementById("location").value;
-  var apiUrl =
-    "https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=$9e7d1b0a51b2cdd53ceb30a255a08f48&units=imperial";
-}
+// function getWeather() {
+//   var apiKey = "9e7d1b0a51b2cdd53ceb30a255a08f48";
+//   var location = document.getElementById("location").value;
+//   var apiUrl =
+//     "https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=$9e7d1b0a51b2cdd53ceb30a255a08f48&units=imperial";
+// }
 
-$(document).ready(function) () {
- 
-    $("#basic-text1").on("click"), function (event) {
-      event.preventDefault();
-      var cityInput = $("#input").val();
-      var allCities = [];
-  
-      allCities = JSON.parse(localStorage.getItem("allCities")) || [];
-      allCities.push(cityInput);
-      localStorage.setItem("allCities", JSON.stringify(allCities));
-      showWeather(cityInput);
-    }};
+// function (cityInput) {
+//     $("#searchBtn").on("click"), function (event) {
+//       event.preventDefault();
+//       var cityInput = $("#input").val();
+//       var allCities = [];
 
+//       allCities = JSON.parse(localStorage.getItem("allCities")) || [];
+//       allCities.push(cityInput);
+//       localStorage.setItem("allCities", JSON.stringify(allCities));
+
+//       showWeather(cityInput);
+//     });
 
 $("dailyWeather").empty();
 $("#fiveDay").empty();
