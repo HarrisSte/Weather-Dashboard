570953dc9bcbfc8d6cb44b1f5ee3eddf6c13fe0f
Stephanie <126029841+HarrisSte@users.noreply.github.com>
2023-04-23 16:16:03 -0400

    Added dayjs to js

--- Assets/script.js
+++ Assets/script.js
@@ -1,3 +1,10 @@
+//Variables
+var currentDate = dayjs().format("YYYY-MM-DD");
+var day1 = dayjs().format("YYYY-MM-DD");
+var day2 = dayjs().format("YYYY-MM-DD");
+var day3 = dayjs().format("YYYY-MM-DD");
+var day4 = dayjs().format("YYYY-MM-DD");
+var day5 = dayjs().format("YYYY-MM-DD");
 //Set up days for weather
 //Set up response for when user clicks/enters city information
 //add WeatherAPI
